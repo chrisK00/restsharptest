f2ccf767f67edf733089d9c0be6032ef68b68019
chris <christianhunter707@gmail.com>
2021-03-26 14:15:16 +0100

    Add project files.

---
+++ restsharptest/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace restsharptest
+{
+    class Program
+    {
+        /// <summary>
+        /// Todo model of jsonplaceholder.api (should be in its own file)
+        /// </summary>
+        public class Todo
+        {
+            public int UserId { get; set; }
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public bool Completed { get; set; }
+        }
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                await PrintTodoAsync();
+                await PrintTodoandResponseAsync();
+                PrintJsonTodo();
+                await AddTodoAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}\n{e.StackTrace}");
+            }          
+        }
+
+        private static async Task PrintTodoAsync()
+        {
+            var client = new RestClient("https://jsonplaceholder.typicode.com/");
+            var request = new RestRequest("todos/1");
+            var todo = await client.GetAsync<Todo>(request);
+            Console.WriteLine(todo.Title);
+        }
+
+        private static async Task PrintTodoandResponseAsync()
+        {
+            var client = new RestClient("https://jsonplaceholder.typicode.com/");
+            var request = new RestRequest("todos/1");
+            var response = await client.ExecuteAsync<Todo>(request);
+            var todo = response.Data;
+            Console.WriteLine(todo.Title);
+        }
+
+        private static void PrintJsonTodo()
+        {
+            var client = new RestClient("https://jsonplaceholder.typicode.com/");
+            var request = new RestRequest("todos/1");
+            var response = client.Get(request);
+            Console.WriteLine(response.Content);
+        }
+
+        private static async Task AddTodoAsync()
+        {
+            var todo = new Todo { Title = "add this todo" };
+            var client = new RestClient("https://jsonplaceholder.typicode.com/");
+            var request = new RestRequest().AddJsonBody(todo);
+            var response = await client.PostAsync<Todo>(request);
+            Console.WriteLine(response.Title);
+        }
+    }
+}
